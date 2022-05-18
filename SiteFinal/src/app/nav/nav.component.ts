ac12ddfec37446d3abac49697ff9e48fcba05e87
ni-nick <60885142+ni-nick@users.noreply.github.com>
2022-05-17 22:24:00 -0300

    site pronto

--- SiteFinal/src/app/nav/nav.component.ts
+++ SiteFinal/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -10,6 +11,7 @@ export class NavComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
